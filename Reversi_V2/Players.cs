cd0452fe9dd7f1cbf630abdebe9e6fc12fbd636c
Jukis252 <74786908+Jukis252@users.noreply.github.com>
2021-11-06 22:57:15 +0200

    Add files via upload

---
+++ Reversi_V2/Players.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi_V2
+{
+    public class Players
+    {
+        int row =0;
+        int col = 0;
+        public void WhitePlayer(int row, int col)
+        {
+            Console.WriteLine("Input row: ");
+            row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input column: ");
+            col = Convert.ToInt32(Console.ReadLine());
+            
+        }
+        public void BlackPlayer(int row, int col)
+        {
+            Console.WriteLine("Input row: ");
+            row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input column: ");
+            col = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Reversi_V2/Work.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversi_V2
+{
+    public class Work
+    {
+        Board board = new Board();
+        public Work()
+        {
+            int run = 1;
+            while(60<run)
+            {
+                Console.Clear();
+                board.ShowBoard();
+                Console.WriteLine("");
+            }
+        }
+    }
+}
